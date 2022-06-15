159ec331888f0e64b8b39d89a148e2ad07330429
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-15 15:53:39 -0300

    Corrigido erro teste - expected 201 but was 200

--- src/main/java/com/gfalencar/libraryapi/api/resource/BookController.java
+++ src/main/java/com/gfalencar/libraryapi/api/resource/BookController.java
@@ -1,15 +1,15 @@
 package com.gfalencar.libraryapi.api.resource;
 
 import com.gfalencar.libraryapi.api.dto.BookDTO;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
 //Criando a rota de POST
     @PostMapping // Anotação que se trata de um método POST
+    @ResponseStatus(HttpStatus.CREATED)
     public BookDTO create(){
         return null;
     }
