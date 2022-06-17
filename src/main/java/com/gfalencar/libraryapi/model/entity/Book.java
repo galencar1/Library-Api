379e6adc93f3ca45321e414f275157379ccfb9ed
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-17 11:38:21 -0300

    Verificando a existencia de um livro pelo ISBN na base de dados

--- src/main/java/com/gfalencar/libraryapi/model/entity/Book.java
+++ src/main/java/com/gfalencar/libraryapi/model/entity/Book.java
@@ -1,17 +1,27 @@
 package com.gfalencar.libraryapi.model.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import javax.persistence.*;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table
 public class Book {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String title;
+
+    @Column
     private String author;
+
+    @Column
     private String isbn;
 }
