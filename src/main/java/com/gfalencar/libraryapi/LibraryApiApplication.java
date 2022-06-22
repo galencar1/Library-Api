aed08c1b7a4e007fe51bac3c66073667a38ce399
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-22 13:56:18 -0300

    Testando o envio de emails

--- src/main/java/com/gfalencar/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/gfalencar/libraryapi/LibraryApiApplication.java
@@ -1,19 +1,37 @@
 package com.gfalencar.libraryapi;
 
+import com.gfalencar.libraryapi.service.EmailService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 @EnableScheduling
 public class LibraryApiApplication {
 
+	@Autowired
+	private EmailService emailService;
+
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
+	}
+
+	@Bean
+	public CommandLineRunner runner(){
+		return args -> {
+			List<String> emails = Arrays.asList("36a569606c-d98ad1@inbox.mailtrap.io");
+			emailService.sendMails("testando serviço de emails.", emails);
+			System.out.println("EMAILS ENVIADOS");
+		};
 	}
 
 	public static void main(String[] args) {
