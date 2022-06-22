e1b2b8dfb79247dfb34c86e7a92bcf8d400296db
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-22 11:49:13 -0300

    Configurando o servidor MailTrap para envio de emails

---
+++ src/main/java/com/gfalencar/libraryapi/service/impl/EmailServiceImpl.java
@@ -0,0 +1,12 @@
+package com.gfalencar.libraryapi.service.impl;
+
+import com.gfalencar.libraryapi.service.EmailService;
+
+import java.util.List;
+
+public class EmailServiceImpl implements EmailService {
+    @Override
+    public void sendMails(String mensagem, List<String> mailsList) {
+
+    }
+}
